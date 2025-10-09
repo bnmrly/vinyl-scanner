ac7b3931b6f4615eb9a8124b4de6829af6fede69
bnmrly <benjaminjohnmarley@gmail.com>
2025-10-09 18:04:25 +0100

    set up Nativewind

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
+import "./global.css";
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>* Hello world</Text>
+    <View style={styles.container} className="border-4 border-blue-500">
+      <Text className="text-red-500"> Hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
