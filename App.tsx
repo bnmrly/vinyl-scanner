1475a027331d296b43b8f37ddf3e893b155ab666
bnmrly <benjaminjohnmarley@gmail.com>
2025-10-15 16:16:07 +0100

    clean up

--- App.tsx
+++ App.tsx
@@ -1,22 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 import "./global.css";
 
 export default function App() {
   return (
-    <View style={styles.container} className="border-4 border-blue-500">
-      <Text className="text-red-500"> Hello world</Text>
+    <View className="border-4 border-blue-500 flex-1 items-center justify-center">
+      <Text className="text-red-500">Hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
