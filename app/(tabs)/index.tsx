3db7dd4377fbe0fbe5a26762fc4443ae3a2577f8
bnmrly <benjaminjohnmarley@gmail.com>
2025-11-04 17:09:34 +0000

    remove stylesheet

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,36 +1,9 @@
-import { StyleSheet } from "react-native";
-
 import { Text, View } from "@/components/Themed";
-
-// TODO: Above import prevents tailwind colour class being added to <Text />
-// import { Text, View } from "react-native";
 
 export default function Scan() {
   return (
-    <View className="border border-red-500" style={styles.container}>
-      <Text className=" bg-yellow-200">scanner here</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
+    <View className="flex-1 border border-orange-500">
+      <Text className="">Scan barcodes here</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
-  },
-});
